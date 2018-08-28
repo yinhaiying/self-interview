d54daa6cb471456e30558a03347a89dddd8b63e6
yinhaiying <>
2018-08-28 13:51:01 +0800

    选择排序

--- javascript/Sort/03selectSort.js
+++ javascript/Sort/03selectSort.js
@@ -9,14 +9,16 @@ let arr = [8,3,0,14,7,5,9,11,1,111,43,56];
 function selectSort(arr){
   var len = arr.length;
   for (let i = 0;i < len;i++) {
-    //定义初始的最大值的位置
+    //定义初始的最大值的位置，一般设置为第一个位置
     let maxIndex = 0;
+
     //第二次的终点位置应该是除去上一步已经排完序的位置，因此终点是len-i
     for(let j = 1;j < len - i ;j++ ){
       if(arr[maxIndex] < arr[j]){
         maxIndex = j;
       }
     }
+
     //将最大的元素与最后一个元素进行交换
     let temp = arr[len-i-1];
         arr[len-i-1] = arr[maxIndex];
