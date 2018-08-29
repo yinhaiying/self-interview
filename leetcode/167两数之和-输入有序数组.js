8b3e57fa1c82725ca72f3cc829e8c898ac6ac560
yinhaiying <>
2018-08-29 14:18:54 +0800

    168使用对撞指针的方

--- leetcode/167两数之和-输入有序数组.js
+++ leetcode/167两数之和-输入有序数组.js
@@ -6,7 +6,24 @@ var twoSum = function(numbers, target) {
     }
   }
 };
+
+//对撞指针方法：设定两个指针，这两个指针在向中间对撞。
+var twoSum2 = function(numbers,target){
+  var l = 0,
+      r = numbers.length - 1;//定义两个指针
+  while(l < r){
+    //如果l=r的话，就只剩下一个元素了，因此不可以相等
+    if(numbers[l] + numbers[r] === target){
+      return [l+1,r+1];
+    }else if(numbers[l] + numbers[r] < target){
+      l++;
+    }else if(numbers[l] + numbers[r] > target) {
+      r--;
+    }
+  }
+
+}
 var numbers = [2,7,11,15];
 var target = 9;
 
-console.log(twoSum(numbers,target))
+console.log(twoSum2(numbers,target))
